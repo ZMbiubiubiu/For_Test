6aada343a11890fc0078c3769aed2196ef7186b2
bingo <390047718@qq.com>
2019-07-20 22:52:15 +0800

    美化

--- geekbang_zuoer/blog/app/__init__.py
+++ geekbang_zuoer/blog/app/__init__.py
@@ -8,12 +8,16 @@
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 from flask_migrate import Migrate
+from flask_mail import Mail
 from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
+mail = Mail(app)
+bootstap = Bootstrap(app)
 db = SQLAlchemy(app)
 db.init_app(app=app)
 with app.app_context():
--- geekbang_zuoer/blog/config.py
+++ geekbang_zuoer/blog/config.py
@@ -14,3 +14,10 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'ksjdflasjdflksaj'
     POSTS_PER_PAGE = 25
+    # 电子邮件的配置
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['zhangmeng.lee@foxmail.com']
